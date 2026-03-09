b4b65fba126f9cc2f78831cd401e9502ce0982fc
Azetus <izana723@hotmail.com>
2026-03-09 14:15:08 +0800

    FloatMenuOptionProvider_GenericPackForApparel

---
+++ Source/FloatMenuOptions/FloatMenuOptionProvider_GenericPackForApparel.cs
@@ -0,0 +1,63 @@
+﻿using ACC_ApparelContainerCore.Comps;
+using ACC_ApparelContainerCore.DefOfs;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ACC_ApparelContainerCore.FloatMenuOptions;
+
+public class FloatMenuOptionProvider_GenericPackForApparel : FloatMenuOptionProvider
+{
+    public override bool Drafted => true;
+    public override bool Undrafted => true;
+    public override bool Multiselect => false;
+    public override bool MechanoidCanDo => true;
+    public override bool RequiresManipulation => true;
+
+    public override bool SelectedPawnValid(Pawn pawn, FloatMenuContext context)
+    {
+        if (!base.SelectedPawnValid(pawn, context))
+            return false;
+        var containers = pawn.apparel?.WornApparel
+            .Where(a => a.TryGetComp<Comp_GenericPackForApparel>() != null)
+            .ToList();
+        return !containers.NullOrEmpty();
+    }
+
+    public override bool TargetThingValid(Thing thing, FloatMenuContext context)
+    {
+        if (!base.TargetThingValid(thing, context))
+            return false;
+        if(context.FirstSelectedPawn is Pawn pawn && thing.IsForbidden(pawn))
+            return false;
+        return Comp_GenericPackForApparel.IsValidTargetToLoadBase(thing);
+    }
+
+    // 返回右键菜单选项
+    public override IEnumerable<FloatMenuOption> GetOptionsFor(Thing clickedThing, FloatMenuContext context)
+    {
+        Pawn pawn = context.FirstSelectedPawn;
+        if (pawn == null || clickedThing == null) yield break;
+        var containers = pawn.apparel?.WornApparel
+            .Where(a => a.HasComp<Comp_GenericPackForApparel>())
+            .ToList();
+
+        if (containers == null || containers.NullOrEmpty()) yield break;
+        // 为每一个容器生成一个独立的 FloatMenuOption
+        foreach (var container in containers)
+        {
+            if(container == null) continue;
+            var comp = container.TryGetComp<Comp_GenericPackForApparel>();
+            // 只显示没装满的容器
+            if (!comp.CanAcceptMore) continue;
+            string label = $"Loading into {container.Label}";
+            yield return new FloatMenuOption(label, () =>
+            {
+                Job job = JobMaker.MakeJob(ACC_JobDefOfs.ACC_Job_PutInGenericPackForApparel, clickedThing,
+                    container);
+                job.count = 1;
+                pawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+            });
+        }
+    }
+}
