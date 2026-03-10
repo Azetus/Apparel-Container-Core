4bfcefadb37176f70b001baedc881072e40bba42
Azetus <izana723@hotmail.com>
2026-03-10 13:51:42 +0800

    JobDriver_ThingHolderContainer

--- Source/JobDrivers/JobDriver_PutInGenericPackForApparel.cs
+++ Source/JobDrivers/JobDriver_PutInGenericPackForApparel.cs
@@ -1,4 +1,5 @@
 ﻿using ACC_ApparelContainerCore.Comps;
+using ACC_ApparelContainerCore.Comps.Props;
 using RimWorld;
 using Verse;
 using Verse.AI;
@@ -6,60 +7,11 @@ using Verse.AI;
 
 namespace ACC_ApparelContainerCore.JobDrivers;
 
-public class JobDriver_PutInGenericPackForApparel : JobDriver
+public class JobDriver_PutInGenericPackForApparel : JobDriver_ThingHolderContainer<Apparel, CompProperties_GenericPackForApparel,
+    Comp_GenericPackForApparel>
 {
-    protected Thing? TargetThing => job.targetA.Thing;
-    protected Thing? TargetPack => job.targetB.Thing;
-
-    public override bool TryMakePreToilReservations(bool errorOnFailed)
+    protected override int PerformLoad(Apparel thing, Comp_GenericPackForApparel comp, int count)
     {
-        // 同时预留物品和包，防止包在走路过程中被脱掉
-        return pawn.Reserve(TargetThing, job, 1, -1, null, errorOnFailed) &&
-               pawn.Reserve(TargetPack, job, 1, -1, null, errorOnFailed);
-    }
-
-    public override IEnumerable<Toil> MakeNewToils()
-    {
-        this.FailOn(() =>
-        {
-            var targetAThing = TargetA.Thing;
-            var targetBThing = TargetB.Thing;
-
-            return targetBThing == null || targetBThing.Destroyed ||
-                   !targetBThing.HasComp<Comp_GenericPackForApparel>() || targetAThing == null ||
-                   targetAThing.Destroyed || targetAThing is not Apparel;
-        });
-
-        // 如果目标物品或者包毁了，停止
-        this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
-        this.FailOnDestroyedOrNull(TargetIndex.B);
-
-        yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
-
-        if (job.count <= 0)
-            job.count = 1;
-        
-        // TODO 这里的 TargetThing 类型校验可能要改一下？
-        if (TargetThing is not Apparel apparel || TargetPack is not ThingWithComps pack ||
-            pack.TryGetComp<Comp_GenericPackForApparel>() is not Comp_GenericPackForApparel comp)
-            yield break;
-        if(!comp.IsTargetInteractable(apparel))
-            yield break;
-
-        Toil putInPack = new Toil();
-        putInPack.initAction = delegate
-        {
-            Pawn actor = putInPack.actor;
-            if (actor != null && apparel != null && comp != null)
-            {
-                int actuallyAdded = comp.TryLoadInto(apparel, job.count);
-                if (actuallyAdded > 0)
-                    Messages.Message($"loading {actuallyAdded} x {apparel.LabelNoCount} into {pack.LabelCap}",
-                        MessageTypeDefOf.PositiveEvent);
-                else
-                    Messages.Message("pack is full or unable to load item", MessageTypeDefOf.RejectInput);
-            }
-        };
-        yield return putInPack;
+        return comp.TryLoadInto(thing, job.count);
     }
 }
---
+++ Source/JobDrivers/JobDriver_ThingHolderContainer.cs
@@ -0,0 +1,104 @@
+﻿using ACC_ApparelContainerCore.Comps;
+using ACC_ApparelContainerCore.Comps.Props;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ACC_ApparelContainerCore.JobDrivers;
+
+public abstract class JobDriver_ThingHolderContainer<TThing, TProps, TComp> : JobDriver
+    where TThing : Thing
+    where TProps : CompProperties_ThingHolderContainer
+    where TComp : Comp_ThingHolderContainer<TThing, TProps>
+{
+    protected Thing? TargetThing => job.targetA.Thing;
+    protected Thing? TargetContainer => job.targetB.Thing;
+    protected TComp? ContainerComp => TargetContainer?.TryGetComp<TComp>();
+    
+    public override bool TryMakePreToilReservations(bool errorOnFailed)
+    {
+        // 同时预留物品和包，防止包在走路过程中被脱掉
+        return pawn.Reserve(job.targetA.Thing, job, 1, -1, null, errorOnFailed) &&
+               pawn.Reserve(job.targetB.Thing, job, 1, -1, null, errorOnFailed);
+    }
+    
+    // 校验job的相关target和comp
+    protected virtual bool ValidateTargets()
+    {
+        return TargetThing != null && 
+               TargetContainer != null && 
+               ContainerComp != null &&
+               !TargetThing.Destroyed && 
+               !TargetContainer.Destroyed &&
+               TargetThing is TThing;
+    }
+    
+    // 失败条件
+    protected virtual void AddFailConditions()
+    {
+        this.FailOn(() => !ValidateTargets());
+        this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
+        this.FailOnDestroyedOrNull(TargetIndex.B);
+    }
+    
+    // 调用ACC_Comp的目标校验
+    protected virtual bool CanLoadIntoContainer(TThing thing, TComp comp)
+    {
+        return comp.IsTargetInteractable(thing);
+    }
+
+    // 装载目标
+    protected abstract int PerformLoad(TThing thing, TComp comp, int count);
+
+    
+    // 装载完成后执行的内容
+    protected virtual void OnLoadComplete(int amountLoaded, TThing thing, Thing container)
+    {
+        if (amountLoaded > 0)
+        {
+            Messages.Message(
+                $"Loading {amountLoaded} x {thing.LabelNoCount} into {container.LabelCap}",
+                MessageTypeDefOf.PositiveEvent
+            );
+        }
+        else
+        {
+            Messages.Message(
+                "Container is full or unable to load item",
+                MessageTypeDefOf.RejectInput
+            );
+        }
+    }
+    
+    
+    public override IEnumerable<Toil> MakeNewToils()
+    {
+        // 失败条件
+        AddFailConditions();
+
+        // 走到物品位置
+        yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
+
+        // 设置默认计数
+        if (job.count <= 0)
+            job.count = 1;
+
+        // 创建装载物品的Toil
+        Toil loadToil = new Toil
+        {
+            initAction = () =>
+            {
+                var thing = TargetThing;
+                var comp = ContainerComp;
+            
+                if (thing is not TThing thingToLoad || comp is not TComp targetComp || !CanLoadIntoContainer(thingToLoad, targetComp))
+                    return;
+
+                int loaded = PerformLoad(thingToLoad, targetComp, job.count);
+                OnLoadComplete(loaded, thingToLoad, TargetContainer!);
+            }
+        };
+
+        yield return loadToil;
+    }
+}
