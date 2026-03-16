b50b5537fe26ac4c0e321875721502fc103070e5
Azetus <izana723@hotmail.com>
2026-03-16 23:04:23 +0800

    ConfigErrors for CompProperties_ThingHolderContainer

--- Source/Comps/Props/CompProperties_ThingHolderContainer.cs
+++ Source/Comps/Props/CompProperties_ThingHolderContainer.cs
@@ -6,7 +6,7 @@ namespace ACC_ApparelContainerCore.Comps.Props;
 public abstract class CompProperties_ThingHolderContainer : CompProperties
 {
     public int storageCapacity = 3;
-    
+
     protected virtual StatCategoryDef GetCompPropCapacityStatCategory => StatCategoryDefOf.Apparel;
 
     public override IEnumerable<StatDrawEntry> SpecialDisplayStats(StatRequest req)
@@ -26,5 +26,18 @@ public abstract class CompProperties_ThingHolderContainer : CompProperties
             "ACC_Stats_Capacity_desc".Translate(),
             0
         );
+    }
+
+    public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+    {
+        foreach (var err in base.ConfigErrors(parentDef))
+        {
+            yield return err;
+        }
+
+        if (storageCapacity <= 0)
+        {
+            yield return $"{parentDef.defName}: storageCapacity must be greater than 0. Current value: {storageCapacity}.";
+        }
     }
 }
